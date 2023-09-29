c0ba04fc4bacf2550bf9056635f3f44f27255f53
Usuario <SergioValera33>
2023-09-29 14:13:40 +0200

    1

--- src/es/marquesdecomares/daw/ed/HolaMundo.java
+++ src/es/marquesdecomares/daw/ed/HolaMundo.java
@@ -3,8 +3,7 @@ package es.marquesdecomares.daw.ed;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("HolaMundo");
-		
+		System.out.println("HolaMundo");z
 	}
 
 }
