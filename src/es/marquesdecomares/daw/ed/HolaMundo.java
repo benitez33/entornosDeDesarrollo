eb1f38e3a271149a20de582a0002b1daf6ba2855
Usuario <SergioValera33>
2023-10-03 08:37:03 +0200

    exercise changed

--- src/es/marquesdecomares/daw/ed/HolaMundo.java
+++ src/es/marquesdecomares/daw/ed/HolaMundo.java
@@ -3,7 +3,7 @@ package es.marquesdecomares.daw.ed;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Equipo");
 	 
 	}
 
